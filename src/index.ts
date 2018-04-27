72930b6efe9b237ae308875cdb42f4dd909250eb
Gilad Kutiel <gilad.kutiel@gmail.com>
2018-04-27 23:26:11 +0300

    svg using inkscape

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,41 @@ import * as http from 'http'
 import * as cp from 'child_process'
 import * as fs from 'fs'
 import * as path from 'path'
-import latex = require('latex')
-
-latex('lala');
 
 http.createServer((req, res) => {
-    const latex = `
-    \\documentclass[crop,tikz]{standalone}
-    \\usetikzlibrary{}
-    \\begin{document}
-    \\begin{tikzpicture}
-      \\draw (0,0) -- (10,10);
-      \\fill[green] (0,0) rectangle (20,20);
-    \\end{tikzpicture}
-    \\end{document}    
-    `
-
     const dir = path.join(__dirname, 'tmp')
     const time = process.hrtime()
     const tex = path.join(dir, time + '.tex')
     const pdf = path.join(dir, time + '.pdf')
-    const png = path.join(dir, time + '.png')
+    const svg = path.join(dir, time + '.svg')
 
+    const latex = `
+    \\documentclass[tikz]{standalone}
+    \\usetikzlibrary{}
+    \\begin{document}
+    \\begin{tikzpicture}
+    \\fill[gray] (3,3) rectangle +(5,5);
+    \\fill[green] (0,0) rectangle +(5,5);
+    \\draw[thick] (0,5) -- +(3,3);
+    \\draw[thick] (5,5) -- +(3,3);
+    \\draw[thick] (5,0) -- +(3,3);
+    \\end{tikzpicture}
+    \\end{document}    
+    `
+
+    const pdfLatex =
+        `pdflatex`
+        + ` -interaction=nonstopmode`
+        + ` -output-directory ${dir}`
+        + ` ${tex}`
     try {
         console.log('tex...')
         fs.writeFile(tex, latex, err => {
-            console.log(`pdflatex -interaction=nonstopmode -output-directory ${dir} ${tex}`)
-            cp.exec(`pdflatex -output-directory ${dir} ${tex}`, (err, stdout) => {
-                console.log(`convert ${pdf} ${png}`)
-                cp.exec(`convert ${pdf} ${png}`, (err, stdout) => {
-                    console.log('done')
-                    fs.readFile(png, (err, data) => {
-                        res.end(`err: ${err} data: ${data}`)
+            console.log(pdfLatex)
+            cp.exec(pdfLatex, (err, stdout) => {
+                cp.exec(`inkscape -l ${svg} ${pdf}`, (err, stdout) => {
+                    fs.readFile(svg, (err, data) => {
+                        res.end(data)
                     })
                 })
             })
